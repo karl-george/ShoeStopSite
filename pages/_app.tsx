6614e6b86ad5d6a8d0ccdcdc905cff27b571b848
Karl George <karlgdev@gmail.com>
2023-05-09 13:33:24 +0200

    adding redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import '@/styles/globals.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '@/redux/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>ShoeStop</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
