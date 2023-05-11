68103ffc69460f53f52b15d003d794630b507961
Karl George <karlgdev@gmail.com>
2023-05-11 11:06:20 +0200

    Adding constant data

---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+const genders = [
+  {
+    title: 'Men',
+  },
+  {
+    title: 'Women',
+  },
+  {
+    title: 'Kids',
+  },
+  {
+    title: 'Unisex',
+  },
+];
+
+const prices = [
+  {
+    price: '$0 - $50',
+  },
+  {
+    price: '$51 - $100',
+  },
+  {
+    price: '$101 - $150',
+  },
+  {
+    price: '$150+',
+  },
+];
+
+export { genders, prices };
