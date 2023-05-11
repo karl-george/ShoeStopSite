b06d5e099656e44f7b90a9121f0ff6f08f0c8950
Karl George <karlgdev@gmail.com>
2023-05-11 11:06:10 +0200

    Filling out sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { genders, prices } from '@/constants';
 import Checkbox from './Checkbox';
 
 interface Props {
@@ -9,19 +10,29 @@ function Sidebar({ brands }: Props) {
     <Checkbox label={brand.title} key={brand._id + brand.title} />
   ));
 
+  const genderList = genders.map((gender, idx) => (
+    <Checkbox label={gender.title} key={gender.title + idx} />
+  ));
+
+  const priceList = prices.map((price, idx) => (
+    <Checkbox label={price.price} key={price.price + idx} />
+  ));
+
   return (
-    <div className='px-6 py-4 space-y-8'>
-      <div className='space-y-3'>
+    <div className='px-6 py-4 divide-y'>
+      <div className='space-y-3 py-4'>
         <h3 className='text-title'>Brand</h3>
         {brandList}
       </div>
-      <div>
+      <div className='space-y-3 py-4'>
         <h3 className='text-title'>Gender</h3>
+        {genderList}
       </div>
-      <div>
+      <div className='space-y-3 py-4'>
         <h3 className='text-title'>Price</h3>
+        {priceList}
       </div>
-      <div>
+      <div className='space-y-3 py-4'>
         <h3 className='text-title'>Color</h3>
       </div>
     </div>
