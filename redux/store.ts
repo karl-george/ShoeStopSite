28c06d138beda2f1129fae579dcf56e367456c92
Karl George <karlgdev@gmail.com>
2023-05-29 15:18:50 +0200

    Adding basket reducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './productSlice';
+import basketReducer from './basketSlice';
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    basket: basketReducer,
   },
 });
 
